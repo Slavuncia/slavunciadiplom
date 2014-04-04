b716178cbc9dc92d6ff4294546cfa2e72187dd85
Slavuncia Carry <slavuncia@mail.ru>
2014-04-04 12:02:00 +0300

    first

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,13 +1,30 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - About';
+$this->pageTitle=Yii::app()->name . ' - Совместимость';
 $this->breadcrumbs=array(
-	'О нас',
+	'Совместимость тонера',
 );
 ?>
-<h1>О нас</h1>
+<h1>Совместимость тонера</h1>
 
-<p>Тут типо о нас
-    
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<div> 
+<p><font color="#00FF00"> "Отлично" </font></p>
+<p><font color="#FFD700"> "Хорошо" </font></p>
+<p><font color="#ff0064"> "Плохо" </font></p>
+<p align="center"><font color="##0000FF">HP/Canon</font></p>
+<p>HP Q2612/ Canon 703/ Canon FX10 - <font color="#00FF00">(HP 1010/ Can 703)</font></p>
+<p>HP Q2612/ Canon 703/ Canon FX10 - <font color="#FFD700">(HP 1100)</font></p>
+<p>HP Q2612/ Canon 703/ Canon FX10 - <font color="#FFD700">(HP 1200)</font></p>
+<p>HP Q4092/ Canon EP22 - <font color="#00FF00">(HP 1100)</font></p>
+<p>HP Q4092/ Canon EP22 - <font color="#FFD700">(HP 1000/ 1200)</font></p>
+<p>HP Q7115. Canon EP27 - <font color="#00FF00">(HP 1000/ 1200)</font></p>
+<p>Q3906 - <font color="#00FF00">(HP 1100)</font></p>
+<p>Canon E16/E30 - <font color="#00FF00">(Canon FC/ PC)</font></p>
+<p>HP CB435/CB436/CE285/CE278/ Canon 712/713/725/728 - <font color="#00FF00">(HP 1005/1006/1505)</font></p>
+<p>HP CB435/CB436/CE285/CE278/ Canon 712/713/725/728 - <font color="#ff0064">(HP 2015)</font></p>
+
+
+</div>
+
+
